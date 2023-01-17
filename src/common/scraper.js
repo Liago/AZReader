e3ab96a0647bb6e6eee30ff81689aac7ce0afd09
Andrea <andrea@zampierolo.dev>
2023-01-17 16:55:00 +0100

    adding some scraper config

--- src/common/scraper.js
+++ src/common/scraper.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { forEach } from 'lodash';
 
 import { getScraperParmas } from '../utility/utils';
 
@@ -22,57 +21,51 @@ export const personalScraper = (url) => {
 				console.log('Esempio =>', {
 					etimo: $('.word-etimo').text(),
 					titolo: $(scraperParams.items.title).html(),
-					contenutop: $(scraperParams.items.content).html().replaceAll('iframe', '')
+					contenutop: $(scraperParams.items.content).html().replaceAll('iframe', ''),
+					pageTitle: $("meta[property='og:image']").attr("content")
 				})
 				// _lead_image_url = $(this).find('meta[property="og:image"]').attr('content')
 			}
 
-
-
 			const title = $(scraperParams.items.title).text();
-			const author = $(scraperParams.items.author).text();	
+			const author = $(scraperParams.items.author).text();
 			const dek = null;
 			const direction = 'ltr';
 			const domain = scraperParams.url;
 			const excerpt = $(scraperParams.items.excerpt).text();
-			if (scraperParams.url === 'www.lescienze.it') {
-				console.log('scraperParams.items.lead_image', scraperParams.items.lead_image)
-				_lead_image_url = $(scraperParams.items.lead_image).find('img').attr('data-src');
-				_date_published = $(scraperParams.items.data_published).attr('datetime');
-			} else {
-				_lead_image_url = $(scraperParams.items.lead_image).find('img').attr('src');
-				_date_published = $(scraperParams.items.data_published).text();
+
+			switch (scraperParams.url) {
+				case 'www.lescienze.it':
+					_lead_image_url = $(scraperParams.items.lead_image).find('img').attr('data-src');
+					_date_published = $(scraperParams.items.data_published).attr('datetime');
+					break;
+				case 'unaparolaalgiorno.it':
+					_lead_image_url = $("meta[property='og:image']").attr("content");
+					break;
+				default:
+					_lead_image_url = $(scraperParams.items.lead_image).find('img').attr('src');
+					_date_published = $(scraperParams.items.data_published).text();
+					break;
 			}
+
 			const date_published = _date_published;
 			const lead_image_url = _lead_image_url;
 			const next_page_url = null;
 			const rendered_pages = 1;
 			const total_pages = 1;
-			
-			if (scraperParams.isHTML) {
-				_content = $(scraperParams.items.content).html().replaceAll('iframe', '');
-			} else {
-				_content = $(scraperParams.items.content).text().replaceAll('\n', '<div class="py-1">');
-			}
+
+			scraperParams.isHTML
+				? _content = $(scraperParams.items.content).html().replaceAll('iframe', '')
+				: _content = $(scraperParams.items.content).text().replaceAll('\n', '<div class="py-1">');
+
 			const jQuery = cheerio.load(_content);
 			jQuery("img").each(function () {
-				let data_source =$(this).attr('data-src');
-				let source = $(this).attr('src');
-				let text = $(this).attr('class');
-				let html = $(this).html();
-				console.log('immagine :>> ', {
-					data_source: data_source,
-					source: source,
-					text: text,
-					html: html
-				});
-				
-				jQuery(this).html().replaceAll(source, data_source)
+				let data_source = $(this).attr('data-src');
+				jQuery(this).attr("src", data_source);
 
 			});
-
-			const content = _content;
-			const word_count = _content.trim().split(/\s+/).length;
+			const content = jQuery.html();
+			const word_count = content.trim().split(/\s+/).length;
 			article.push({
 				author,
 				content,
@@ -89,8 +82,6 @@ export const personalScraper = (url) => {
 				url,
 				word_count
 			})
-
-			console.log('articole', article[0])
 			return article;
 		}).catch(err => console.log(err))
 }
