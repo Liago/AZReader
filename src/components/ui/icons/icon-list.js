58d0ed29f2557e4e378550c0424eb0fd4ccffdfc
Andrea <andrea@zampierolo.dev>
2023-03-23 14:55:26 +0100

    icon components

--- src/components/ui/icons/icon-list.js
+++ src/components/ui/icons/icon-list.js
@@ -1,6 +1,6 @@
-export const IconList = () => {
+export const IconList = ({ className }) => {
 	return (
-		<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+		<svg className={className}
 			xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
 			<path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
 		</svg>
--- src/components/ui/icons/index.js
+++ src/components/ui/icons/index.js
@@ -1,2 +1,3 @@
 export { IconAllElements } from "./icon-all";
 export { IconList } from "./icon-list";
+export { IconAddUsers } from './icon-add-users';
