0ae104afb9a4a22b67480adc8c9ee8ff0159eb78
Andrea <andrea@zampierolo.dev>
2024-09-05 14:32:44 +0200

    feat: add capacitor to get mail link

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,53 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
-const config: CapacitorConfig = {
+interface IntentFilter {
+	action: string;
+	autoVerify?: boolean;
+	data: Array<{
+		scheme: string;
+		host: string;
+		pathPrefix: string;
+	}>;
+	category: string[];
+}
+
+type ExtendedAndroidConfig = NonNullable<CapacitorConfig["android"]> & {
+	intentFilters?: IntentFilter[];
+};
+
+interface ExtendedCapacitorConfig extends Omit<CapacitorConfig, "android"> {
+	android?: ExtendedAndroidConfig;
+}
+
+const config: ExtendedCapacitorConfig = {
 	appId: "io.ionic.starter",
 	appName: "AZReader",
 	webDir: "build",
 	bundledWebRuntime: false,
-	server: { iosScheme: "ionic" },
+	plugins: {
+		CapacitorHttp: {
+			enabled: true,
+		},
+	},
+	android: {
+		intentFilters: [
+			{
+				action: "VIEW",
+				autoVerify: true,
+				data: [
+					{
+						scheme: "azreader",
+						host: "auth",
+						pathPrefix: "/confirm",
+					},
+				],
+				category: ["BROWSABLE", "DEFAULT"],
+			},
+		],
+	},
+	ios: {
+		scheme: "azreader",
+	},
 };
 
 export default config;
