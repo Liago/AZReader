287f0d51b98fa3662f700ebee914c7bd9cb0802a
Andrea <andrea@zampierolo.dev>
2024-12-18 15:33:59 +0100

    feat: homepage transitioned from js to typescript

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,48 +1,54 @@
 import { useRef } from "react";
 import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import { powerOutline, logInOutline, documentTextOutline } from "ionicons/icons";
-
 import MainMenu from "../components/ui/menu";
 import ModalParser from "../components/modalParser";
 import { Auth } from "../components/form/authentication";
 import ArticleList from "../components/articleList";
-
 import useAuth from "../hooks/useAuth";
 import useArticles from "../hooks/useArticles";
-
+import { Session } from "@supabase/auth-js/dist/module/lib/types";
+import { ArticleParsed } from "../common/interfaces";
 import "./Home.css";
 
-const Home = () => {
+interface ModalProps {
+	articleParsed: ArticleParsed | null;
+	showModal: boolean;
+	pageRef: React.RefObject<HTMLElement>;
+	savePostHandler: () => void;
+	setShowModal: (show: boolean) => void;
+	searchText: string;
+	setSearchText: (text: string) => void;
+	savePostToServer: () => void;
+	loading: boolean;
+	isParsing: boolean;
+}
+
+const Home: React.FC = () => {
 	const { session, signOut } = useAuth();
-	const {
-		showModal, setShowModal,
-		searchText, setSearchText,
-		isParsing, articleParsed,
-		savePostHandler, savePostToServer,
-		loading
-	} = useArticles(session);
+	const { showModal, setShowModal, searchText, setSearchText, isParsing, articleParsed, savePostHandler, savePostToServer, loading } = useArticles(
+		session as Session | null
+	);
 
-	const pageRef = useRef();
+	const pageRef = useRef<HTMLElement>(null);
 
-	const renderTitle = () => session ? 'Articoli condivisi' : 'I miei articoli';
+	const renderTitle = (): string => (session ? "Articoli condivisi" : "I miei articoli");
 
-	const renderContent = () => {
+	const renderContent = (): JSX.Element => {
 		if (!session) {
 			return <Auth />;
 		}
 
 		return (
 			<>
-				<ArticleList session={session} />
+				<ArticleList session={session as Session} />
 				{renderModalParser()}
 			</>
 		);
 	};
 
-	const renderModalParser = () => {
-		if (isParsing) return null;
-
-		const modalProps = {
+	const renderModalParser = (): JSX.Element | null => {
+		const modalProps: ModalProps = {
 			articleParsed,
 			showModal,
 			pageRef,
@@ -51,7 +57,8 @@ const Home = () => {
 			searchText,
 			setSearchText,
 			savePostToServer,
-			loading
+			loading,
+			isParsing,
 		};
 
 		return <ModalParser {...modalProps} />;
@@ -64,28 +71,26 @@ const Home = () => {
 				<IonHeader>
 					<IonToolbar>
 						<IonButtons slot="start">
-							<IonMenuButton></IonMenuButton>
+							<IonMenuButton />
 						</IonButtons>
 						<IonTitle>{renderTitle()}</IonTitle>
 						<IonButtons slot="primary">
 							{session ? (
 								<IonButton color="dark" onClick={signOut}>
-									<IonIcon slot='icon-only' icon={powerOutline} />
+									<IonIcon slot="icon-only" icon={powerOutline} />
 								</IonButton>
 							) : (
 								<IonButton color="dark" onClick={() => setShowModal(true)}>
-									<IonIcon slot='icon-only' icon={logInOutline} />
+									<IonIcon slot="icon-only" icon={logInOutline} />
 								</IonButton>
 							)}
 							<IonButton color="dark" onClick={() => setShowModal(true)}>
-								<IonIcon slot='icon-only' icon={documentTextOutline} />
+								<IonIcon slot="icon-only" icon={documentTextOutline} />
 							</IonButton>
 						</IonButtons>
 					</IonToolbar>
 				</IonHeader>
-				<IonContent fullscreen>
-					{renderContent()}
-				</IonContent>
+				<IonContent fullscreen>{renderContent()}</IonContent>
 			</IonPage>
 			{/* {session && <Account session={session} />} */}
 		</>
