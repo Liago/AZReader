6f6ac141c583dc36ef2f715f476105ca6e83ecb1
Andrea <andrea@zampierolo.dev>
2025-01-02 15:44:19 +0100

    feat: added Font Size globally styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
 			open_sans: ['"Open Sans"', 'sans-serif'],
 			roboto: ['"Roboto Slab"', 'serif'],
 		},
+		fontSize: {
+			'xs': ['0.75rem', { lineHeight: '1rem' }],
+			'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+			'base': ['1rem', { lineHeight: '1.5rem' }],
+			'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+			'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+			'2xl': ['1.5rem', { lineHeight: '2rem' }],
+		},
 	},
 	plugins: [
 		function ({ addUtilities }) {
