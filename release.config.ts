dbf86c4b82caca464480023148cae6c352fcb344
Andrea <andrea@zampierolo.dev>
2025-01-13 17:24:39 +0100

    fix release config

--- release.config.ts
+++ release.config.ts
@@ -3,8 +3,8 @@ import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 
-export default {
-	branches: ["dev", "main"],
+const releaseConfig = {
+	branches: ["dev", "master"],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
@@ -17,13 +17,29 @@ export default {
 		"@semantic-release/npm",
 		[
 			"@semantic-release/exec",
+			{
+				prepareCmd: `
+		  # Update iOS version
+		  /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString \${nextRelease.version}" ios/App/App/Info.plist
+		  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion \${nextRelease.version}" ios/App/App/Info.plist
+		  
+		  # Update Android version
+		  sed -i '' 's/versionName "[^"]*"/versionName "\${nextRelease.version}"/' android/app/build.gradle
+		  
+		  # Calculate Android version code (remove dots and convert to integer)
+		  VERSION_CODE=$(echo \${nextRelease.version} | sed 's/\\.//g')
+		  sed -i '' 's/versionCode [0-9]*/versionCode '\${VERSION_CODE}'/' android/app/build.gradle
+		`,
+			},
 		],
 		[
 			"@semantic-release/git",
 			{
-				assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+				assets: ["package.json", "package-lock.json", "CHANGELOG.md", "ios/App/App/Info.plist", "android/app/build.gradle"],
 				message: "v${nextRelease.version}",
 			},
 		],
 	],
 };
+
+export default releaseConfig;
